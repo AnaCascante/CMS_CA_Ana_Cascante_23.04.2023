b4610fc778686bcde08ad6f06dc8ba164beea7c5
AnaCascante <ana.cascante@stud.noroff.no>
2023-04-23 21:15:53 +0200

    just to be sure
    in case I lose conection

--- js/products.js
+++ js/products.js
@@ -19,3 +19,47 @@ getProducts();
     function createHTML(result){
         for(let i = 0; i < result.length; i++) {
             const list = result[i];
+
+        
+});``
+
+/* for fetching a single product*/
+
+fetch('https://rainydays23.learnbydoing.online/wp-json/wp/v2/products/{product_id}')
+  .then(response => response.json())
+  .then(product => console.log(product))
+  .catch(error => console.error(error));
+
+
+  /* creating a functionality to render a grid of product data in HTML as thumbnails */
+
+fetch('https://rainydays23.learnbydoing.online/wp-json/wp/v2/products')
+  .then(response => response.json())
+  .then(products => {
+    const container = document.getElementById('product-grid');
+
+    // Loop through the products array and create a thumbnail for each product
+    products.forEach(product => {
+     
+      const thumbnail = document.createElement('a');
+      thumbnail.classList.add('product-thumbnail');
+      thumbnail.href = product.link;
+
+      // Create an image element and set its src and alt attributes
+      const image = document.createElement('img');
+      image.src = product.image.src;
+      image.alt = product.image.alt;
+
+      // Create a title element and set its text content
+      const title = document.createElement('h3');
+      title.textContent = product.title.rendered;
+
+      // Append the image and title elements to the thumbnail element
+      thumbnail.appendChild(image);
+      thumbnail.appendChild(title);
+
+      // Append the thumbnail element to the container element
+      container.appendChild(thumbnail);
+    });
+  })
+  .catch(error => console.error(error));
